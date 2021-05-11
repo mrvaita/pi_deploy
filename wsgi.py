543601724b2d1d393c1a68121cc185f650c6b231
mrvaita <michele.tassoni@gmail.com>
2021-05-11 08:55:30 +0200

    webhook secured

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,11 @@
+import os
 from app import app
+from dotenv import load_dotenv
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
 
 if __name__ == "__main__":
     app.run()
